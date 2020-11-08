496176f76dc69580d79845c47cf914ea67f8082a
132nd.Neck <neck.dcs@gmail.com>
2020-11-08 12:06:55 +0100

    chg: debug skynet

--- l10n/DEFAULT/skynet-iads-setup-OPAR.lua
+++ l10n/DEFAULT/skynet-iads-setup-OPAR.lua
@@ -4,6 +4,22 @@ do
 redIADS = SkynetIADS:create('SYRIA')
 
 
+---debug settings remove from here on if you do not wan't any output on what the IADS is doing by default
+local iadsDebug = redIADS:getDebugSettings()
+iadsDebug.IADSStatus = true
+iadsDebug.samWentDark = true
+iadsDebug.contacts = true
+iadsDebug.radarWentLive = true
+iadsDebug.noWorkingCommmandCenter = true
+iadsDebug.ewRadarNoConnection = true
+iadsDebug.samNoConnection = true
+iadsDebug.jammerProbability = true
+iadsDebug.addedEWRadar = true
+iadsDebug.hasNoPower = true
+iadsDebug.harmDefence = true
+---end remove debug ---
+
+
 --add all units with unit name beginning with 'EWR' to the IADS:
 redIADS:addEarlyWarningRadarsByPrefix('EWR')
 
