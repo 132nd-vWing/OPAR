5ff9d89468af7bf2c8861f540409bd9a4c8fb88c
132nd.Neck <neck.dcs@gmail.com>
2022-05-05 10:53:29 +0200

    chg: Start OPAR v2

--- l10n/DEFAULT/OPAR_NavyGroups.lua
+++ l10n/DEFAULT/OPAR_NavyGroups.lua
@@ -1,11 +1,11 @@
 carrier_root_menu = MENU_MISSION:New("CSG_72_Abraham_Lincoln")
-carrier_root_menu1 = MENU_MISSION:New("CSG_73_George_Washington")
+--carrier_root_menu1 = MENU_MISSION:New("CSG_73_George_Washington")
 
 --- CVN72 Supercarrier ---
-CVN72 = NAVYGROUP:New("CSG_72_1")
+CVN72 = NAVYGROUP:New("CVN_72_ABRAHAM_LINCOLN")
 CVN72:SetPatrolAdInfinitum()
 CVN72:SetDefaultROE(ENUMS.ROE.WeaponFree)
-CVN_72_beacon_unit = UNIT:FindByName("CARRIER MOTHER 309.2")
+CVN_72_beacon_unit = UNIT:FindByName("CVN_72_ABRAHAM_LINCOLN")
 if CVN_72_beacon_unit then
   CVN72_Beacon = CVN_72_beacon_unit:GetBeacon()
   CVN72_Beacon:ActivateICLS(12,"C72")
@@ -13,15 +13,15 @@ if CVN_72_beacon_unit then
 end
 
 --- CVN73 Supercarrier ---
-CVN73 = NAVYGROUP:New("CVN_73_GEORGE_WASHINGTON")
-CVN73:SetPatrolAdInfinitum()
-CVN73:SetDefaultROE(ENUMS.ROE.WeaponFree)
-CVN_73_beacon_unit = UNIT:FindByName("CVN_73_GEORGE_WASHINGTON")
-if CVN_73_beacon_unit then
-  CVN73_Beacon = CVN_73_beacon_unit:GetBeacon()
-  CVN73_Beacon:ActivateICLS(13,"C73")
-  env.info("CVN73 ICLS started on channel 13")
-end
+--CVN73 = NAVYGROUP:New("CVN_73_GEORGE_WASHINGTON")
+--CVN73:SetPatrolAdInfinitum()
+--CVN73:SetDefaultROE(ENUMS.ROE.WeaponFree)
+--CVN_73_beacon_unit = UNIT:FindByName("CVN_73_GEORGE_WASHINGTON")
+--if CVN_73_beacon_unit then
+--  CVN73_Beacon = CVN_73_beacon_unit:GetBeacon()
+--  CVN73_Beacon:ActivateICLS(13,"C73")
+--  env.info("CVN73 ICLS started on channel 13")
+--end
 
 
 
@@ -34,13 +34,13 @@ SCHEDULER:New(nil,function()
   end
 end,{},5,5*60)
 
-SCHEDULER:New(nil,function()
-  if CVN_73_beacon_unit then
-    CVN73_Beacon = CVN_73_beacon_unit:GetBeacon()
-    CVN73_Beacon:ActivateTACAN(73,"X","C73",true)
-    env.info("CVN73 Tacan refreshed")
-  end
-end,{},5,5*60)
+--SCHEDULER:New(nil,function()
+--  if CVN_73_beacon_unit then
+--    CVN73_Beacon = CVN_73_beacon_unit:GetBeacon()
+--    CVN73_Beacon:ActivateTACAN(73,"X","C73",true)
+--    env.info("CVN73 Tacan refreshed")
+--  end
+--end,{},5,5*60)
 
 
 function start_recovery()
@@ -58,18 +58,18 @@ end
 
 
 
-function start_recovery1()
-  if CVN73:IsSteamingIntoWind() == true then
-    MessageToAll("CVN-73 is already recovering, recovery window will closes at time "..timerecovery_end)
-  else
-    local timenow=timer.getAbsTime( )
-    local timeend=timenow+45*60
-    local timerecovery_start = UTILS.SecondsToClock(timenow,true)
-    timerecovery_end = UTILS.SecondsToClock(timeend,true)
-    CVN73:AddTurnIntoWind(timerecovery_start,timerecovery_end,25,true,-9)
-    MessageToAll("CVN73 is turning, Recovery Window open from time "..timerecovery_start.." until "..timerecovery_end)
-  end
-end
+--function start_recovery1()
+--  if CVN73:IsSteamingIntoWind() == true then
+--    MessageToAll("CVN-73 is already recovering, recovery window will closes at time "..timerecovery_end)
+--  else
+--    local timenow=timer.getAbsTime( )
+--    local timeend=timenow+45*60
+--    local timerecovery_start = UTILS.SecondsToClock(timenow,true)
+--    timerecovery_end = UTILS.SecondsToClock(timeend,true)
+--    CVN73:AddTurnIntoWind(timerecovery_start,timerecovery_end,25,true,-9)
+--    MessageToAll("CVN73 is turning, Recovery Window open from time "..timerecovery_start.." until "..timerecovery_end)
+--  end
+--end
 menu_start_recovery = MENU_MISSION_COMMAND:New("Start Recovery at CVN72",carrier_root_menu,start_recovery)
-menu_start_recovery1 = MENU_MISSION_COMMAND:New("Start Recovery at CVN73",carrier_root_menu1,start_recovery1)
+--menu_start_recovery1 = MENU_MISSION_COMMAND:New("Start Recovery at CVN73",carrier_root_menu1,start_recovery1)
 
