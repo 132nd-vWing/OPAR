87b42f0bd5ab3cc201fc7eed051cea75dae30e94
132nd.Neck <neck.dcs@gmail.com>
2022-05-06 22:45:04 +0200

    chg: Added revised version of skynet

--- l10n/DEFAULT/skynet-iads-setup-OPAR_1st_corps.lua
+++ l10n/DEFAULT/skynet-iads-setup-OPAR_1st_corps.lua
@@ -20,6 +20,36 @@ iadsDebug.earlyWarningRadarStatusEnvOutput = true
 iadsDebug.commandCenterStatusEnvOutput = true
 ---end remove debug ---
 
+--add SA6 BN
+
+redIADS:addSAMSitesByPrefix('1ST_SA6BN')
+
+--add SA11 BN
+redIADS:addSAMSitesByPrefix('1ST_SA11_Batt1')
+
+redIADS:addSAMSitesByPrefix('1ST_SA11_Batt2')
+
+
+--set SA-6 site to always work as EWR
+redIADS:getSAMSiteByGroupName('1ST_SA6BN'):samSite:setActAsEW(true)
+
+
+-- Go active at 70 percent
+redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_1'):setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE):setGoLiveRangeInPercent(70)
+
+redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_2'):setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE):setGoLiveRangeInPercent(70)
+
+redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_3'):setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE):setGoLiveRangeInPercent(70)
+
+redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_4'):setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE):setGoLiveRangeInPercent(70)
+
+redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_5'):setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE):setGoLiveRangeInPercent(70)
+
+redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_6'):setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE):setGoLiveRangeInPercent(70)
+
+redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_7'):setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE):setGoLiveRangeInPercent(70)
+
+redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_8'):setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE):setGoLiveRangeInPercent(70)
 
 --add all units with unit name beginning with 'EWR' to the IADS:
 redIADS:addEarlyWarningRadarsByPrefix('FIRSTSBORKA')
