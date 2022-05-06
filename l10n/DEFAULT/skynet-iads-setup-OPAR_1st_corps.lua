16165eb62f1791a0f742cc16cf8a1e3684458374
132nd.Neck <neck.dcs@gmail.com>
2022-05-06 17:14:35 +0200

    chg: adjusted skynet

--- l10n/DEFAULT/skynet-iads-setup-OPAR_1st_corps.lua
+++ l10n/DEFAULT/skynet-iads-setup-OPAR_1st_corps.lua
@@ -2,6 +2,24 @@ do
 --create an instance of the IADS
 redIADS = SkynetIADS:create('1stCorps')
 
+---debug settings remove from here on if you do not wan't any output on what the IADS is doing by default
+local iadsDebug = redIADS:getDebugSettings()
+iadsDebug.IADSStatus = true
+iadsDebug.radarWentDark = true
+iadsDebug.contacts = true
+iadsDebug.radarWentLive = true
+iadsDebug.noWorkingCommmandCenter = false
+iadsDebug.ewRadarNoConnection = false
+iadsDebug.samNoConnection = false
+iadsDebug.jammerProbability = true
+iadsDebug.addedEWRadar = false
+iadsDebug.hasNoPower = false
+iadsDebug.harmDefence = true
+iadsDebug.samSiteStatusEnvOutput = true
+iadsDebug.earlyWarningRadarStatusEnvOutput = true
+iadsDebug.commandCenterStatusEnvOutput = true
+---end remove debug ---
+
 --add all units with unit name beginning with 'EWR' to the IADS:
 redIADS:addEarlyWarningRadarsByPrefix('1ST_SBORKA')
 
@@ -44,6 +62,7 @@ redIADS:getSAMSiteByGroupName('AD_1ST_SA15_7'):setEngagementZone(SkynetIADSAbstr
 redIADS:getSAMSiteByGroupName('AD_1ST_SA15_8'):setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE):setGoLiveRangeInPercent(70)
 
 
+redIADS:addRadioMenu()
 
 --activate the IADS
 redIADS:activate()
