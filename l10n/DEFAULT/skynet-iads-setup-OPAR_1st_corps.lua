50eea7b28c281a50b9129994215225eaa88f8fff
132nd.Neck <neck.dcs@gmail.com>
2022-05-07 21:05:56 +0200

    chg: Remove debug skynet

--- l10n/DEFAULT/skynet-iads-setup-OPAR_1st_corps.lua
+++ l10n/DEFAULT/skynet-iads-setup-OPAR_1st_corps.lua
@@ -2,24 +2,6 @@ do
 --create an instance of the IADS
 redIADS = SkynetIADS:create('FIRSTCORPS')
 
----debug settings remove from here on if you do not wan't any output on what the IADS is doing by default
-local iadsDebug = redIADS:getDebugSettings()
-iadsDebug.IADSStatus = true
-iadsDebug.radarWentDark = true
-iadsDebug.contacts = true
-iadsDebug.radarWentLive = true
-iadsDebug.noWorkingCommmandCenter = false
-iadsDebug.ewRadarNoConnection = false
-iadsDebug.samNoConnection = false
-iadsDebug.jammerProbability = true
-iadsDebug.addedEWRadar = false
-iadsDebug.hasNoPower = false
-iadsDebug.harmDefence = true
-iadsDebug.samSiteStatusEnvOutput = true
-iadsDebug.earlyWarningRadarStatusEnvOutput = true
-iadsDebug.commandCenterStatusEnvOutput = true
----end remove debug ---
-
 --add SA6 BN
 
 redIADS:addSAMSitesByPrefix('FIRST_SA6BN')
