7b3101187208bf6b014d671d766ed21e505a9b76
132nd.Neck <neck.dcs@gmail.com>
2022-05-26 12:46:16 +0200

    chg: Fixes in Skynet

--- l10n/DEFAULT/skynet-iads-setup-OPAR_1st_corps.lua
+++ l10n/DEFAULT/skynet-iads-setup-OPAR_1st_corps.lua
@@ -12,7 +12,6 @@ redIADS:addSAMSitesByPrefix('FIRST_SA11_Batt1')
 redIADS:addSAMSitesByPrefix('FIRST_SA11_Batt2')
 
 
-
 --add all units with unit name beginning with 'EWR' to the IADS:
 redIADS:addEarlyWarningRadarsByPrefix('FIRSTSBORKA')
 
@@ -22,6 +21,13 @@ redIADS:addSAMSitesByPrefix('FIRSTAD')
 --add a command center:
 commandCenter = StaticObject.getByName('FIRSTCORPSCOMMANDCENTER')
 redIADS:addCommandCenter(commandCenter)
+
+
+redIADS:getSAMSiteByGroupName('FIRST_SA11_Batt1'):setGoLiveRangeInPercent(80):setHARMDetectionChance(50)
+
+redIADS:getSAMSiteByGroupName('FIRST_SA11_Batt2'):setGoLiveRangeInPercent(80):setHARMDetectionChance(50)
+
+redIADS:getSAMSiteByGroupName('FIRST_SA6BN'):setGoLiveRangeInPercent(80):setHARMDetectionChance(50)
 
 
 --activate the IADS
