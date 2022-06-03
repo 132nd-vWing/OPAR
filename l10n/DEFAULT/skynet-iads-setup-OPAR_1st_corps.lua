288551da422e485fb1e772ba21d3d7664550eb5f
132nd.Neck <neck.dcs@gmail.com>
2022-06-03 20:33:38 +0200

    chg: Fixed error in scripts (Airboss and Skynet).

--- l10n/DEFAULT/skynet-iads-setup-OPAR_1st_corps.lua
+++ l10n/DEFAULT/skynet-iads-setup-OPAR_1st_corps.lua
@@ -2,14 +2,6 @@ do
 --create an instance of the IADS
 redIADS = SkynetIADS:create('FIRSTCORPS')
 
---add SA6 BN
-redIADS:addSAMSitesByPrefix('FIRST_SA6BN')
-
---add SA11 BN
-redIADS:addSAMSitesByPrefix('FIRST_SA11_Batt1')
-
-redIADS:addSAMSitesByPrefix('FIRST_SA11_Batt2')
-
 
 --add all units with unit name beginning with 'EWR' to the IADS:
 redIADS:addEarlyWarningRadarsByPrefix('FIRSTSBORKA')
@@ -21,48 +13,6 @@ redIADS:addSAMSitesByPrefix('FIRSTAD')
 commandCenter = StaticObject.getByName('FIRSTCORPSCOMMANDCENTER')
 redIADS:addCommandCenter(commandCenter)
 
-
-redIADS:getSAMSiteByGroupName('FIRST_SA11_Batt1'):setGoLiveRangeInPercent(80):setHARMDetectionChance(50)
-
-redIADS:getSAMSiteByGroupName('FIRST_SA11_Batt2'):setGoLiveRangeInPercent(80):setHARMDetectionChance(50)
-
-redIADS:getSAMSiteByGroupName('FIRST_SA6BN'):setGoLiveRangeInPercent(80):setHARMDetectionChance(50)
-
-
--- POINT DEFENSES
-local sa15 = redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_10')
-redIADS:getEarlyWarningRadarByUnitName('FIRSTSBORKA_10th'):addPointDefence(sa15)
-
-local sa15 = redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_7')
-redIADS:getEarlyWarningRadarByUnitName('FIRSTSBORKA_11th'):addPointDefence(sa15)
-
-local sa15 = redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_6')
-redIADS:getEarlyWarningRadarByUnitName('FIRSTSBORKA_11th'):addPointDefence(sa15)
-
-local sa15 = redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_1')
-redIADS:getEarlyWarningRadarByUnitName('FIRSTSBORKA_12th'):addPointDefence(sa15)
-
-local sa15 = redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_3')
-redIADS:getEarlyWarningRadarByUnitName('FIRSTSBORKA_12th'):addPointDefence(sa15)
-
-
-
-local sa15 = redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_5')
-redIADS:getSAMSiteByGroupName('FIRST_SA11_Batt1'):addPointDefence(sa15)
-
-local sa15 = redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_8')
-redIADS:getSAMSiteByGroupName('FIRST_SA11_Batt1'):addPointDefence(sa15)
-
-
-local sa15 = redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_11')
-redIADS:getSAMSiteByGroupName('FIRST_SA11_Batt2'):addPointDefence(sa15)
-
-local sa15 = redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_12')
-redIADS:getSAMSiteByGroupName('FIRST_SA11_Batt2'):addPointDefence(sa15)
-
-
-local sa15 = redIADS:getSAMSiteByGroupName('FIRSTAD_1ST_SA15_9')
-redIADS:getSAMSiteByGroupName('FIRST_SA6BN'):addPointDefence(sa15)
 
 --activate the IADS
 redIADS:activate()
--- l10n/DEFAULT/skynet-iads-setup-OPAR_2st_corps.lua
+++ l10n/DEFAULT/skynet-iads-setup-OPAR_2st_corps.lua
@@ -4,6 +4,7 @@ redIADS = SkynetIADS:create('SECONDCORPS')
 
 
 
+
 --add all units with unit name beginning with 'EWR' to the IADS:
 redIADS:addEarlyWarningRadarsByPrefix('SECONDSBORKA')
 
@@ -13,31 +14,6 @@ redIADS:addSAMSitesByPrefix('SECONDAD')
 --add a command center:
 commandCenter = StaticObject.getByName('SECONDCORPSCOMMANDCENTER')
 redIADS:addCommandCenter(commandCenter)
-
-
--- POINT DEFENSES
-local sa15 = redIADS:getSAMSiteByGroupName('SECONDAD_2ND_SA15_5')
-redIADS:getEarlyWarningRadarByUnitName('SECONDSBORKA_22nd'):addPointDefence(sa15)
-
-local sa15 = redIADS:getSAMSiteByGroupName('SECONDAD_2ND_SA15_8')
-redIADS:getEarlyWarningRadarByUnitName('SECONDSBORKA_22nd'):addPointDefence(sa15)
-
-
-local sa15 = redIADS:getSAMSiteByGroupName('SECONDAD_2ND_SA15_4')
-redIADS:getEarlyWarningRadarByUnitName('SECONDSBORKA_21st'):addPointDefence(sa15)
-
-local sa15 = redIADS:getSAMSiteByGroupName('SECONDAD_2ND_SA15_3')
-redIADS:getEarlyWarningRadarByUnitName('SECONDSBORKA_21st'):addPointDefence(sa15)
-
--- ACTIVATION RANGE
-redIADS:getSAMSiteByGroupName('SECONDAD_2ND_SA8_1'):setGoLiveRangeInPercent(85):setHARMDetectionChance(80)
-redIADS:getSAMSiteByGroupName('SECONDAD_2ND_SA8_2'):setGoLiveRangeInPercent(85):setHARMDetectionChance(80)
-redIADS:getSAMSiteByGroupName('SECONDAD_2ND_SA8_3'):setGoLiveRangeInPercent(85):setHARMDetectionChance(80)
-redIADS:getSAMSiteByGroupName('SECONDAD_2ND_SA8_4'):setGoLiveRangeInPercent(85):setHARMDetectionChance(80)
-redIADS:getSAMSiteByGroupName('SECONDAD_2ND_SA8_5'):setGoLiveRangeInPercent(85):setHARMDetectionChance(80)
-redIADS:getSAMSiteByGroupName('SECONDAD_2ND_SA8_6'):setGoLiveRangeInPercent(85):setHARMDetectionChance(80)
-redIADS:getSAMSiteByGroupName('SECONDAD_2ND_SA8_7'):setGoLiveRangeInPercent(85):setHARMDetectionChance(80)
-redIADS:getSAMSiteByGroupName('SECONDAD_2ND_SA8_7'):setGoLiveRangeInPercent(85):setHARMDetectionChance(80)
 
 
 
