8baeb76c33554931ab62d86c4fc780f705eb8b9a
132nd.Neck <neck.dcs@gmail.com>
2022-09-11 08:35:25 +0200

    chg: Updated skynet

--- l10n/DEFAULT/skynet-iads-setup-OPAR.lua
+++ l10n/DEFAULT/skynet-iads-setup-OPAR.lua
@@ -2,6 +2,25 @@ do
 --create an instance of the IADS
 redIADS = SkynetIADS:create('SYRIA')
 
+
+---debug settings remove from here on if you do not wan't any output on what the IADS is doing by default
+local iadsDebug = redIADS:getDebugSettings()
+iadsDebug.IADSStatus = true
+iadsDebug.radarWentDark = true
+iadsDebug.contacts = true
+iadsDebug.radarWentLive = true
+iadsDebug.noWorkingCommmandCenter = true
+iadsDebug.ewRadarNoConnection = true
+iadsDebug.samNoConnection = true
+iadsDebug.jammerProbability = true
+iadsDebug.addedEWRadar = true
+iadsDebug.hasNoPower = true
+iadsDebug.harmDefence = true
+iadsDebug.samSiteStatusEnvOutput = true
+iadsDebug.earlyWarningRadarStatusEnvOutput = true
+iadsDebug.commandCenterStatusEnvOutput = true
+---end remove debug ---
+
 --add all units with unit name beginning with 'EWR' to the IADS:
 redIADS:addEarlyWarningRadarsByPrefix('EWR')
 
@@ -18,29 +37,29 @@ redIADS:addCommandCenter(commandCenter)
 --EWR CONNECTION TO SECTOR COMMAND CENTER - WEST
 --add a power source and a connection node for this EW radar:
 local connectionNodeEW = StaticObject.getByName('SCC_WEST')
-redIADS:getEarlyWarningRadarByUnitName('EWR_W_1-1'):addPowerSource(powerSource):addConnectionNode(connectionNodeEW)
+redIADS:getEarlyWarningRadarByUnitName('EWR_W_1-1'):addConnectionNode(connectionNodeEW)
 
 ----add a power source and a connection node for this EW radar :
 local connectionNodeEW = StaticObject.getByName('SCC_WEST')
-redIADS:getEarlyWarningRadarByUnitName('EWR_W_2-1'):addPowerSource(powerSource):addConnectionNode(connectionNodeEW)
+redIADS:getEarlyWarningRadarByUnitName('EWR_W_2-1'):addConnectionNode(connectionNodeEW)
 
 ----add a power source and a connection node for this EW radar:
 local connectionNodeEW = StaticObject.getByName('SCC_WEST')
-redIADS:getEarlyWarningRadarByUnitName('EWR_W_3-1'):addPowerSource(powerSource):addConnectionNode(connectionNodeEW)
+redIADS:getEarlyWarningRadarByUnitName('EWR_W_3-1'):addConnectionNode(connectionNodeEW)
 
 
 -- EWR CONNECTION TO SECTOR COMMAND CENTER - EAST
 --add a power source and a connection node for this EW radar:
 local connectionNodeEW = StaticObject.getByName('SCC_EAST')
-redIADS:getEarlyWarningRadarByUnitName('EWR_E_1-1'):addPowerSource(powerSource):addConnectionNode(connectionNodeEW)
+redIADS:getEarlyWarningRadarByUnitName('EWR_E_1-1'):addConnectionNode(connectionNodeEW)
 
 ----add a power source and a connection node for this EW radar:
 local connectionNodeEW = StaticObject.getByName('SCC_EAST')
-redIADS:getEarlyWarningRadarByUnitName('EWR_E_2-1'):addPowerSource(powerSource):addConnectionNode(connectionNodeEW)
+redIADS:getEarlyWarningRadarByUnitName('EWR_E_2-1'):addConnectionNode(connectionNodeEW)
 
 ----add a power source and a connection node for this EW radar:
 local connectionNodeEW = StaticObject.getByName('SCC_EAST')
-redIADS:getEarlyWarningRadarByUnitName('EWR_E_3-1'):addPowerSource(powerSource):addConnectionNode(connectionNodeEW)
+redIADS:getEarlyWarningRadarByUnitName('EWR_E_3-1'):addConnectionNode(connectionNodeEW)
 
 
 
@@ -68,8 +87,8 @@ redIADS:getEarlyWarningRadarByUnitName('EWR_E_3-1'):addPowerSource(powerSource):
 
 -- POINT DEFENCE SECTOR WEST
 -- SA-2 Al Qusair
---local sa15 = redIADS:getSAMSiteByGroupName('IADS_W_SA15_PD_1')
---redIADS:getSAMSiteByGroupName('IADS_W_SA2_1'):addPointDefence(sa15):setHARMDetectionChance(100)
+local sa15 = redIADS:getSAMSiteByGroupName('IADS_W_SA15_PD_1')
+redIADS:getSAMSiteByGroupName('IADS_W_SA2_1'):addPointDefence(sa15)
 
 -- SA-2 Al Duhur
 --local sa15 = redIADS:getSAMSiteByGroupName('IADS_W_SA15_PD_3')
@@ -78,8 +97,11 @@ redIADS:getEarlyWarningRadarByUnitName('EWR_E_3-1'):addPowerSource(powerSource):
 --redIADS:getSAMSiteByGroupName('IADS_W_SA2_2'):addPointDefence(sa15):setHARMDetectionChance(100)
 
 -- SA-6 Hama
---local sa15 = redIADS:getSAMSiteByGroupName('IADS_W_SA15_PD_2')
---redIADS:getSAMSiteByGroupName('IADS_W_SA6_1'):addPointDefence(sa15):setHARMDetectionChance(100)
+local sa15 = redIADS:getSAMSiteByGroupName('IADS_W_SA15_PD_2')
+redIADS:getSAMSiteByGroupName('IADS_W_SA6_1'):addPointDefence(sa15)
+
+
+
 
 -- SA-6 Aleppo
 --local sa15 = redIADS:getSAMSiteByGroupName('IADS_W_SA15_PD_4')
@@ -93,22 +115,23 @@ redIADS:getEarlyWarningRadarByUnitName('EWR_E_3-1'):addPowerSource(powerSource):
 
 -- POINT DEFENCE SECTOR EAST
 -- SA-2 Tabqua airfield
---local sa15 = redIADS:getSAMSiteByGroupName('IADS_E_SA15_PD_1')
---redIADS:getSAMSiteByGroupName('IADS_E_SA2_1'):addPointDefence(sa15):setHARMDetectionChance(100)
+local sa15 = redIADS:getSAMSiteByGroupName('IADS_E_SA15_PD_1')
+redIADS:getSAMSiteByGroupName('IADS_E_SA2_1'):addPointDefence(sa15)
 
 -- SA-2 Jirah Airfield
---local sa15 = redIADS:getSAMSiteByGroupName('IADS_E_SA15_PD_2')
---redIADS:getSAMSiteByGroupName('IADS_E_SA2_2'):addPointDefence(sa15):setHARMDetectionChance(100)
---local sa15 = redIADS:getSAMSiteByGroupName('IADS_E_SA15_PD_5')
---redIADS:getSAMSiteByGroupName('IADS_E_SA2_2'):addPointDefence(sa15):setHARMDetectionChance(100)
+local sa15 = redIADS:getSAMSiteByGroupName('IADS_E_SA15_PD_2')
+redIADS:getSAMSiteByGroupName('IADS_E_SA2_2'):addPointDefence(sa15)
+
+local sa15 = redIADS:getSAMSiteByGroupName('IADS_E_SA15_PD_5')
+redIADS:getSAMSiteByGroupName('IADS_E_SA2_2'):addPointDefence(sa15)
 
 -- SA-6 TABQA Airfield
---local sa15 = redIADS:getSAMSiteByGroupName('IADS_E_SA15_PD_3')
---redIADS:getSAMSiteByGroupName('IADS_E_SA6_1'):addPointDefence(sa15):setHARMDetectionChance(100)
+local sa15 = redIADS:getSAMSiteByGroupName('IADS_E_SA15_PD_3')
+redIADS:getSAMSiteByGroupName('IADS_E_SA6_1'):addPointDefence(sa15)
 
--- SA-6 KUWEIRES airfield
---local sa15 = redIADS:getSAMSiteByGroupName('IADS_E_SA15_PD_4')
---redIADS:getSAMSiteByGroupName('IADS_E_SA6_2'):addPointDefence(sa15):setHARMDetectionChance(100)
+-- SA-6 KUWEIRES airfield (MOVED TO ALEPPO for D3.1)
+local sa15 = redIADS:getSAMSiteByGroupName('IADS_E_SA15_PD_4')
+redIADS:getSAMSiteByGroupName('IADS_E_SA6_2'):addPointDefence(sa15)
 
 -- GO active at 60%
 --redIADS:getSAMSiteByGroupName('IADS_E_SA2_1'):setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE):setGoLiveRangeInPercent(70)
